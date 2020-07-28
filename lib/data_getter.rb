ef71f85185bb88e152ad21d13842a353b2bc360b
mavrik83 <rjhuber83@gmail.com>
2020-07-28 15:58:45 -0400

    adds the DataGetter class

--- lib/data_getter.rb
+++ lib/data_getter.rb
@@ -1,2 +1,20 @@
+require_relative "./console_control"
+require_relative "./data_handler"
+require 'pry'
+require "httparty"
 
+class DataGetter
+
+    
+
+    def get_categories
+        HTTParty.get("https://api.chucknorris.io/jokes/categories")
+    end
+
+    def choose_category(category)
+        HTTParty.get("https://api.chucknorris.io/jokes/random?category=#{category}")
+    end
+
+
+end
 
