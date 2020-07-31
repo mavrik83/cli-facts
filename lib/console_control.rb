a114785f520c475d49d827a06d9f6717f1dac5c4
mavrik83 <rjhuber83@gmail.com>
2020-07-31 14:00:18 -0400

    adds #list_builder method to ConsoleControl

--- lib/console_control.rb
+++ lib/console_control.rb
@@ -1,14 +1,27 @@
-
 class ConsoleControl
+    # declares and freezes acceptable input responses for menu navigation
+    AFFIRMATIVE_INPUT = ["yes", "y"].freeze
+    NEGATIVE_INPUT = ["no", "n"].freeze
+    QUIT_INPUT = ["exit", "quit", "q", "x"].freeze
 
+    # On starting app, #run clears the terminal screen, displays a welcome message,
+    # runs #list_builder, which builds the list categories, and then moves the program
+    # to #select_category
     def run
+        system("clear")
         puts "\nWelcome to Chuck Norris facts!"
         puts "This app will generate a random\nChuck Norris FACT by category\n\n"
+        list_builder
+        select_category
+    end
+
+    # Calls the API for an array of categories and assigns it to the variable. Iterates over
+    # the array and instantiates a new object for each element.
+    def list_builder
         list = DataGetter.get_categories
         list.each do |cat|
             CategoryList.new(cat)
         end
-        select_category
     end
 
     def select_category
@@ -24,6 +37,7 @@ class ConsoleControl
         end
     end
 
+    # this method does something
     def fact_parser(category)
         response = DataGetter.choose_category(category)
         response["value"]
@@ -43,10 +57,10 @@ class ConsoleControl
         elsif response.to_i.between?(1, CategoryList.all.length)
             category = CategoryList.all[response.to_i - 1]
             fact_display(category.name)
-        elsif response.downcase == "exit" || response.downcase == "quit"
+        elsif QUIT_INPUT.include?(response.downcase)
             exit_app
         else
-            puts "That isn't one of the categories!"
+            puts "That isn't one of the categories! Please enter\na number or the name of a category."
             fact_input
         end
         self.exit_or_rerun
@@ -56,9 +70,9 @@ class ConsoleControl
         puts "\n\nDo you want to select another category?\n\n"
         print "yes/no? :=> "
         response = gets.chomp
-        if response.downcase == "yes" 
+        if AFFIRMATIVE_INPUT.include?(response.downcase)
             select_category
-        elsif response.downcase == "no" || response.downcase == "exit" || response.downcase == "quit"
+        elsif NEGATIVE_INPUT.include?(response.downcase) || QUIT_INPUT.include?(response.downcase)
             exit_app
         else
             puts "Invalid response! Please enter 'yes', 'no', or 'exit' to quit"
--- lib/data_handler.rb
+++ lib/data_handler.rb
@@ -16,3 +16,4 @@ class CategoryList
 
 end
 
+
