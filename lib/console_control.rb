811fb60a0ce29df942bd67ee2c4e904b3c71ff01
mavrik83 <rjhuber83@gmail.com>
2020-07-31 08:14:11 -0400

    adds UI funcionality

--- lib/console_control.rb
+++ lib/console_control.rb
@@ -13,18 +13,30 @@ class ConsoleControl
     end
 
     def select_category
-        puts "Please select from the following catagories:"
+        puts "Please select from the following catagories:\n\n"
         
         puts DataHandler.category_lister
+        puts "\n\n"
     
         self.display_fact
     end
 
     def display_fact
+        print ":=> "
         response = gets.chomp
+        if response
+
+        end
         fact = DataHandler.fact_parser(response)
+        puts "\n\n"
         puts fact 
     end
+
+    # def exit_or_rerun
+    #     ""
+    #     response = gets.chomp
+    #     if respons 
+    # end
 
     
 
