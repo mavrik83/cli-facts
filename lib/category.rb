bc61cb95d7c1397d9c1112692319e486bf482e4e
mavrik83 <rjhuber83@gmail.com>
2020-07-31 16:57:43 -0400

    added comments to category.rb

--- lib/category.rb
+++ lib/category.rb
@@ -1,14 +1,18 @@
 class Category
 
+    # Getter and setter methods. 
     attr_accessor :name
 
+    # Collection of all instance of categories. i.e. each named category is an object and is collected here.
     @@categories = []
 
+    # Instantiates a new category object and pushed it into the @@categories variable.
     def initialize(name)
         @name = name
         @@categories << self
     end
 
+    # All method to give outside access to the class variable @@categories.
     def self.all
         @@categories
     end
